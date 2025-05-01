63077ed8ea2bd939bc385bd2e1d3971a2d41486d
PrajwalPrabhuiisc <75930064+PrajwalPrabhuiisc@users.noreply.github.com>
2025-05-01 16:19:18 +0530

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -2,4 +2,8 @@ import open3d as o3d
 
 def visualize_point_cloud(pcd, colors):
     pcd.colors = o3d.utility.Vector3dVector(colors)
+    
+    # Optional: Save the point cloud for future use
+    # o3d.io.write_point_cloud("output/processed_point_cloud.ply", pcd)
+    
     o3d.visualization.draw_geometries([pcd], width=800, height=600)
